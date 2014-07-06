e41a9080ed491e5fb106a49ceb90c3788a92a00b
K <k@Ks-iMac.local>
2014-07-06 17:25:49 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -7,7 +7,7 @@
 //
 
 #import "Gameplay.h"
-//#import "Seal.h"
+#import "CCPhysics+ObjectiveChipmunk.h"
 
 @implementation Gameplay{
     CCPhysicsNode *_physicsNode;
@@ -36,7 +36,6 @@
     _mouseJointNode.physicsBody.collisionMask = @[];
     
     _physicsNode.collisionDelegate = self;
-    self.physicsBody.collisionType = @"seal";
 }
 
 // called on every touch in this scene
@@ -133,8 +132,19 @@
     [[CCDirector sharedDirector] replaceScene:[CCBReader loadAsScene:@"Gameplay"]];
 }
 
--(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB{
-    CCLOG(@"Something collided with a seal!");
+- (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB {
+    float energy = [pair totalKineticEnergy];
+    
+    // if energy is large enough, remove the seal
+    if (energy > 5000.f) {
+        [[_physicsNode space] addPostStepBlock:^{
+            [self sealRemoved:nodeA];
+        } key:nodeA];
+    }
+}
+
+- (void)sealRemoved:(CCNode *)seal {
+    [seal removeFromParent];
 }
 
 @end
