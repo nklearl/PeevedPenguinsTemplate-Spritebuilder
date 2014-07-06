359bce77e6e908c7499d69cb3450f7f63a83a92f
K <k@Ks-iMac.local>
2014-07-06 18:17:04 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Seal.m
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -21,9 +21,7 @@
 //}
 
 - (void)didLoadFromCCB{
-    self.physicsBody.collisionType = @"seal";
-    //[self.physicsBody setType:CCPhysicsBodyTypeDynamic];
-    CCLOG(@"seal called");
+    //self.physicsBody.collisionType = @"seal";
 }
 
 @end
