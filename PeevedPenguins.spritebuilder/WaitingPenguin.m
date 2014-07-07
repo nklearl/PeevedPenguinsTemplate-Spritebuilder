8c165af75ab89f14eb4fb820dd3aadeb9368ce7c
K <k@Ks-iMac.local>
2014-07-07 00:10:09 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -13,9 +13,9 @@
 - (void)didLoadFromCCB
 {
     // generate a random number between 0.0 and 2.0
-    //float delay = (arc4random() % 2000) / 1000.f;
+    float delay = (arc4random() % 2000) / 1000.f;
     // call method to start animation after random delay
-    //[self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
+    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
 }
 
 - (void)startBlinkAndJump
