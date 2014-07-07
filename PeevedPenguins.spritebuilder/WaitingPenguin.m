88f0806fb049ce0b5483048a3d6ebede2d625055
K <k@Ks-iMac.local>
2014-07-06 20:04:40 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.m
+//  PeevedPenguins
+//
+//  Created by K on 7/6/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "WaitingPenguin.h"
+
+@implementation WaitingPenguin
+
+@end
